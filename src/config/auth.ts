8d1be6dd27b17b98063b03a9d95742ec3644a6fa
Thales Moreira <thsm271@gmail.com>
2021-01-17 14:14:36 -0300

    first commit

---
+++ src/config/auth.ts
@@ -0,0 +1,6 @@
+export default {
+  jwt: {
+    secret: "e60ff2e01a62a72cec1e9e678853b7db",
+    expiresIn: "1d",
+  },
+};
---
+++ src/config/upload.ts
@@ -0,0 +1,9 @@
+import path from "path";
+import multer from "multer";
+
+export default {
+  storage: multer.diskStorage({
+    destination: path.resolve(__dirname, "..", "..", "tmp"),
+    filename(request, file, callback) {},
+  }),
+};
